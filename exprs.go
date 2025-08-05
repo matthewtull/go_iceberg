e2cbe94792fbadfaeebe1fa793c1ca8845fd270d
Superstar <39806623+kailee55n@users.noreply.github.com>
2025-08-05 16:11:26 -0500

    Update exprs.go
    Co-Authored-By: Christopher Jordan <21223943+christopher862-dev@users.noreply.github.com>

--- exprs.go
+++ exprs.go
@@ -14,6 +14,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+// Package iceberg provides types and functions for working with Apache Iceberg tables.
 
 package iceberg
 
